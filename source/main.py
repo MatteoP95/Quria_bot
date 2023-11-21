ff001f4f8aa692957c7c8e3c55c039224aae3c0e
Mattia Moltisanti <mattia40k@gmail.com>
2023-11-21 20:50:48 +0100

    Feat: Main Update

--- source/main.py
+++ source/main.py
@@ -1,18 +1,24 @@
 import os
-
 import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = discord.Client(intents=discord.Intents.default())
-
 
 @client.event
 async def on_ready():
     print(f"{client.user} has connected to Discord!")
+    print("----------------------------------------")
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello, i am Quria!")
 
 
 client.run(TOKEN)
